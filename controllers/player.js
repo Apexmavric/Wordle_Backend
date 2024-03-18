cf00cd206ca2e63c689957d22a30b03f9c83f97f
Apexmavric <vidhitpatel21@gmail.com>
2024-03-18 19:34:00 +0530

    Authentication and Registeration Working!

--- controllers/player.js
+++ controllers/player.js
@@ -2,7 +2,14 @@ const {StatusCodes} = require('http-status-codes');
 const getDetails = async(req, res)=>{
     res.status(StatusCodes.OK).send('Get details has been called');
 }
-
+const addFriends = async(req, res)=>{
+    res.status(StatusCodes.OK).send('Get AddFriends has been called');
+}
+const updateScore = async(req, res)=>{
+    res.status(StatusCodes.OK).send('Get UpdateScore has been called');
+}
 module.exports = {
-    getDetails
+    getDetails,
+    addFriends,
+    updateScore
 }
